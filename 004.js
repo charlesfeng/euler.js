29560fcbbb4b996fa8751caa5860277c04f3fa31
Charles Feng <charles@charles.io>
2013-05-22 02:03:11 -0600

    problem 4

---
+++ 004.js
@@ -0,0 +1,8 @@
+(function () {
+  for (var p, i = 1998; i >= 200; i--) {
+    for (var j = 999; i - j <= j; j--) {
+      p = i * j
+      if (('' + p).split('').reverse().join('') === '' + p) return p
+    }
+  }
+})()
