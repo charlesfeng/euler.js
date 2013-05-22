aff0af4a1319efcb9e69c4680a9ec0f500fce6ae
Charles Feng <charles@charles.io>
2013-05-22 03:46:23 -0600

    problem 12

--- 012.js
+++ 012.js
@@ -1,8 +1,4 @@
-var d = 0
-  , n = 0
-  , t
-  , i
-  , a
+var d = 0, n = 0, t, i, a
 
 while (d < 500) {
   n++
@@ -14,12 +10,13 @@ while (d < 500) {
   while (t > 1) {
     if (t % i) i++
     else {
-      a[i] = a[i] || 0 + 1
+      t /= i
+      a[i] = (a[i] || 0) + 1
       i = 2
     }
   }
   
-  console.log(a)
-  
   d = Object.keys(a).reduce(function (p, c) { return p * (a[c] + 1) }, 1)
 }
+
+n * (n + 1) / 2
