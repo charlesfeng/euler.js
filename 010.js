384f486bae47feed926e9f62560995d8d48910f9
Charles Feng <charles@charles.io>
2013-05-22 04:00:00 -0700

    fix bug with 10

--- 010.js
+++ 010.js
@@ -1,10 +1,15 @@
+// http://projecteuler.net/problem=10
+
 var a = []
   , i = 2
 
-while (!a.length || a[a.length - 1] < 2000000) {
+while (!a.length || i < 2000000) {
   if (!a.some(function (c) { return !(i % c) }))
     a.push(i)
   i++
 }
 
 a.reduce(function (p, c) { return p + c }, 0)
+
+// answer: 142913828922
+// runtime: 448536 ms
