caa46956f69a36d0f5f3d6f6ff413ce1a869fd8f
Charles Feng <charles@charles.io>
2013-05-23 10:28:45 -0700

    problem 20

--- 020.js
+++ 020.js
@@ -1,10 +1,20 @@
 // http://projecteuler.net/problem=20
 
-var d = Date.now()
+var p = [1]
+  , c = 0
+  , i
 
+for (i = 1; i <= 100; i++) {
+  for (var j = 0; j < p.length; j++) {
+    p[j] = p[j] * i + c
+    c = Math.floor(p[j] / 10)
+    p[j] %= 10
+  }
+  if (c) p = p.concat(('' + c).split('').reverse().map(function (a) { return parseInt(a) }))
+  c = 0
+}
 
+p.reduce(function (q, d) { return q + d }, 0)
 
-console.log(Date.now() - d)
-
-// answer: 
-// runtime:  ms
+// answer: 648
+// runtime: 11 ms
