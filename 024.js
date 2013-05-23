11d5f757e155e70283f1e3521b869c1f539b4d1f
Charles Feng <charles@charles.io>
2013-05-23 15:08:31 -0700

    make 24 a lot shorter

--- 024.js
+++ 024.js
@@ -1,27 +1,22 @@
 // http://projecteuler.net/problem=24
 
 var t = 1000000
-  , f = [1]
-  , p = 0
   , a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  , f = [1, 1]
+  , p = 0
   , c, i
 
 for (i = 2; i < 10; i++)
-  f[i - 1] = f[i - 2] * i
+  f[i] = f[i - 1] * i
 
-for (i = 10; --i; ) {
-  c = 0
-  
-  while (t > f[i - 1]) {
-    t -= f[i - 1]
-    c++
-  }
-  
+for (i = 10; i--; ) {
+  c = Math.floor((t - 1) / f[i])
   p = 10 * p + a[c]
+  t -= c * f[i]
   a.splice(c, 1)
 }
 
-p = 10 * p + a[0]
+p
 
 // answer: 2783915460
 // runtime: 11 ms
