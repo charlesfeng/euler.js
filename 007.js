c1e6ea6ec7dd6122e60cb72661fb6ede3cf5367c
Charles Feng <charles@charles.io>
2013-06-04 23:07:55 -0700

    use primes module for #7

--- 007.js
+++ 007.js
@@ -1,24 +1,32 @@
 // http://projecteuler.net/problem=7
 
-var ns = []
-  , c = 2
-  , n = 0
-  , i
-  
-while (n < 10001) {
-  if (!ns[c]) {
-    n++
-    for (i = 1; i <= Math.floor(200000 / c); i++)
-      ns[i * c] = true
-  }
-  c++
-}
-
-module.exports = c - 1
+module.exports = require('./modules/primes')(120000).ps[10000]
 
 // answer: 104743
-// runtime: 42 ms
+// runtime: 28 ms
 
+//------------------------------------------------------------------------------------------------//
+
+// var ns = []
+//   , c = 2
+//   , n = 0
+//   , i
+  
+// while (n < 10001) {
+//   if (!ns[c]) {
+//     n++
+//     for (i = 1; i <= Math.floor(200000 / c); i++)
+//       ns[i * c] = true
+//   }
+//   c++
+// }
+
+// module.exports = c - 1
+
+// // answer: 104743
+// // runtime: 42 ms
+
+//------------------------------------------------------------------------------------------------//
 
 // var a = []
 //   , i = 2
