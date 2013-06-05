9bbd2a1d518550c235fc0b5666bde9bcd471225d
Charles Feng <charles@charles.io>
2013-06-04 23:21:49 -0700

    add non-prime-number-based factorization method

--- modules/factor.js
+++ modules/factor.js
@@ -1,36 +1,55 @@
-// finds the prime factorization of a number
+// finds the prime factorization of a number using one of two methods
 //
 // parameters:
 //   n - the number
-//   p (optional) - an array s.t. p[n] tells us if n is prime
-//   ps (optional) - an array of prime numbers
+//   ps (optional) - an array of prime numbers, or true to signal that we should use prime numbers
 //
 // returns: the pf as an object of { prime1: number1, ... }
 
 module.exports = function (n, ps) {
   if (n === 1) return { 1: 1 }
-  
+  if (ps === true) ps = require('./primes')(Math.floor(Math.sqrt(n))).ps
+    
   var pf = {}
-    , f = []
-    , t = n
-    , j = 0
   
-  if (!ps) ps = require('./primes')(Math.floor(Math.sqrt(n))).ps
-  
-  while (t > 1 && ps[j] <= Math.sqrt(t)) {
-    if (t % ps[j]) j++
-    else {
-      t /= ps[j]
-      f[j] = (f[j] || 0) + 1
-      j = 0
+  // method 1: iterate through prime numbers
+  if (ps) {
+    var f = []
+      , t = n
+      , j = 0
+    
+    while (t > 1 && ps[j] <= Math.sqrt(t)) {
+      if (t % ps[j]) j++
+      else {
+        t /= ps[j]
+        f[j] = (f[j] || 0) + 1
+        j = 0
+      }
     }
-  }
-  
-  f.forEach(function (c, i) {
-    pf[ps[i]] = c
-  })
+    
+    f.forEach(function (c, i) {
+      pf[ps[i]] = c
+    })
 
-  if (t > 1) pf[t] = 1
+    if (t > 1) pf[t] = 1
+  
+  // method 2: iterate through integers
+  } else {
+    var p = 2
+      , f = {}
+
+    while (p < n) {
+      if (n % p) {
+        p++
+      } else {
+        pf[p] = (pf[p] || 0) + 1
+        n /= p
+        p = 2
+      }
+    }
+    
+    if (p > 1) pf[p] = (pf[p] || 0) + 1
+  }
   
   return pf
 }
