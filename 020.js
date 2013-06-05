c845a73dbe4454bc3114f951a3f7c237b9b2c8dc
Charles Feng <charles@charles.io>
2013-06-05 00:46:16 -0700

    use bigint class in problem 20

--- 020.js
+++ 020.js
@@ -1,20 +1,31 @@
 // http://projecteuler.net/problem=20
 
-var p = [1]
-  , c = 0
-  , i
+var BigInt = require('./modules/bigint')
 
-for (i = 1; i <= 100; i++) {
-  for (var j = 0; j < p.length; j++) {
-    p[j] = p[j] * i + c
-    c = Math.floor(p[j] / 10)
-    p[j] %= 10
-  }
-  if (c) p = p.concat(('' + c).split('').reverse().map(function (a) { return parseInt(a) }))
-  c = 0
-}
-
-module.exports = p.reduce(function (q, d) { return q + d }, 0)
+for (var n = new BigInt(1), i = 2; i <= 100; i++) n.multiply(i)
+  
+module.exports = n.toArray().reduce(function (p, c) { return p + c }, 0)
 
 // answer: 648
-// runtime: 14 ms
+// runtime: 15 ms
+
+//------------------------------------------------------------------------------------------------//
+
+// var p = [1]
+//   , c = 0
+//   , i
+
+// for (i = 1; i <= 100; i++) {
+//   for (var j = 0; j < p.length; j++) {
+//     p[j] = p[j] * i + c
+//     c = Math.floor(p[j] / 10)
+//     p[j] %= 10
+//   }
+//   if (c) p = p.concat(('' + c).split('').reverse().map(function (a) { return parseInt(a) }))
+//   c = 0
+// }
+
+// module.exports = p.reduce(function (q, d) { return q + d }, 0)
+
+// // answer: 648
+// // runtime: 14 ms
