4ad696452d6fe0b4b6003eb37aefe4981414b5f7
Charles Feng <charles@charles.io>
2013-06-04 23:22:02 -0700

    use factorization module for #3

--- 003.js
+++ 003.js
@@ -1,25 +1,25 @@
 // http://projecteuler.net/problem=3
 
-var a = 600851475143
-  , b = 2
-
-while (b < a) {
-  if (a % b) {
-    b++
-  } else {
-    a /= b
-    b = 2
-  }
-}
-
-module.exports = a
+module.exports = Object.keys(require('./modules/factor')(600851475143)).slice(-1)[0]
 
 // answer: 6857
 // runtime: 13 ms
 
 //------------------------------------------------------------------------------------------------//
 
-// module.exports = Object.keys(require('./modules/factor')(600851475143)).slice(-1)[0]
+// var a = 600851475143
+//   , b = 2
+
+// while (b < a) {
+//   if (a % b) {
+//     b++
+//   } else {
+//     a /= b
+//     b = 2
+//   }
+// }
+
+// module.exports = a
 
 // // answer: 6857
-// // runtime: 124 ms
+// // runtime: 13 ms
