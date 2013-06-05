f6c051492592250bf58e498c6cb97fcf0d303b90
Charles Feng <charles@charles.io>
2013-06-04 23:47:35 -0700

    use primes module for problem 10

--- 010.js
+++ 010.js
@@ -1,21 +1,28 @@
 // http://projecteuler.net/problem=10
 
-var ns = []
-  , c = 2
-  , s = 0
-  , i
-  
-while (c < 2000000) {
-  if (!ns[c]) {
-    s += c
-    for (i = 1; i <= Math.floor(2000000 / c); i++)
-      ns[i * c] = true
-  }
-  
-  c++
-}
-
-module.exports = s
+module.exports = require('./modules/primes')(2000000).ps.reduce(function (p, c) { return p + c }, 0)
 
 // answer: 142913828922
-// runtime: 312 ms
+// runtime: 302 ms
+
+//------------------------------------------------------------------------------------------------//
+
+// var ns = []
+//   , c = 2
+//   , s = 0
+//   , i
+  
+// while (c < 2000000) {
+//   if (!ns[c]) {
+//     s += c
+//     for (i = 1; i <= Math.floor(2000000 / c); i++)
+//       ns[i * c] = true
+//   }
+  
+//   c++
+// }
+
+// module.exports = s
+
+// // answer: 142913828922
+// // runtime: 312 ms
