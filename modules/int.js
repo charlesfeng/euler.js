94b2440abe9bfe22a84d23a32de397501b796607
Charles Feng <charles@charles.io>
2013-06-06 00:13:45 -0700

    int module to parallel bigint

---
+++ modules/int.js
@@ -0,0 +1,59 @@
+// int module (to parallel bigint)
+
+// (c) 2013 charles feng (https://github.com/charlesfeng)
+// shared under the mit license (http://www.opensource.org/licenses/mit)
+
+var Int = function (n) {
+  if (n.isInt) this.n = n.n
+  else this.n = n
+}
+
+Int.prototype = {
+    isInt: true
+  
+  // display
+  , toString: function () {
+      return this.n.toString()
+    }
+  , toKey: function () {
+      return this.n
+    }
+  , toNumber: function () {
+      return this.n
+    }
+    
+  // comparison
+  , gt: function (that) {
+      return this.n > that.n
+    }
+  , gte: function (that) {
+      return this.n >= that.n
+    }
+  , lt: function (that) {
+      return this.n < that.n
+    }
+  , lte: function (that) {
+      return this.n <= that.n
+    }
+  , eq: function (that) {
+      return this.n === that.n
+    }
+  , ne: function (that) {
+      return this.n !== that.n
+    }
+  
+  // arithmetic
+  , add: function (n) {
+      return new Int(this.n + (typeof n === 'number' ? n : parseInt(n, 10)))
+    }
+  , multiply: function (n) {
+      return new Int(this.n * n)
+    }
+    
+  // other operators
+  , pow: function (n) {
+      return new Int(Math.pow(this.n, n))
+    }
+}
+
+module.exports = Int
